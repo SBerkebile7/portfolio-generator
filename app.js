1fab6fada915218127e2e6288848edadf466c14b
Spenny Berks <sberkebile@live.com>
2021-06-18 13:47:47 -0400

    installed inquirer

--- app.js
+++ app.js
@@ -1,34 +1,24 @@
+const inquirer = require('inquirer');
+
+inquirer
+    .prompt([
+        {
+            type:'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+
+/*
 const fs = require('fs');
+const generatePage = require('./src/page-template');
 
-const generatePage = require('./src/page-template.js');
+const pageHTML = generatePage(name, github);
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-const [name, github] = profileDataArgs;
-
-
-
-fs.writeFile('./index.html',generatePage(name, github), err => {
-    if(err) throw new Error(err);
+fs.writeFile('./index.html',pageHTML, err => {
+    if(err) throw (err);
 
     console.log('Portfolio complete! Check out index.html to see the output!');
 })
-
-
-
-
-/*
-console.log(profileDataArgs);
-
-const printProfileData = profileDataArr => {
-    for(let i = 0; i < profileDataArr.length; i+=1) {
-        console.log(profileDataArr[i]);
-    }
-
-    console.log('============');
-
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
 */
