b8e4924f6f0d3f2c8d69146da3e5a50439b42a04
Spenny Berks <sberkebile@live.com>
2021-06-18 16:43:13 -0400

    final HTML touches

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const inquirer = require('inquirer');
 
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
+
 const promptUser = () => {
     return inquirer.prompt([
         {
@@ -128,18 +132,12 @@ const promptProject = portofolioData => {
 promptUser()
 .then(promptProject)
 .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html',pageHTML, err => {
+        if(err) throw (err);
+
+        console.log('Portfolio complete! Check out index.html to see the output!');
+    })
 });
 
-/*
-const fs = require('fs');
-const generatePage = require('./src/page-template');
-
-const pageHTML = generatePage(name, github);
-
-fs.writeFile('./index.html',pageHTML, err => {
-    if(err) throw (err);
-
-    console.log('Portfolio complete! Check out index.html to see the output!');
-})
-*/
