cf367c8b1be0d7a75bb8dfe506a3e85eef4c6ef3
Spenny Berks <sberkebile@live.com>
2021-06-18 13:01:01 -0400

    rearranged files and made app.js easier to read

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+const [name, github] = profileDataArgs;
+
+
+
+fs.writeFile('./index.html',generatePage(name, github), err => {
+    if(err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+})
+
+
+
+
+/*
 console.log(profileDataArgs);
 
 const printProfileData = profileDataArr => {
@@ -12,3 +31,4 @@ const printProfileData = profileDataArr => {
 };
 
 printProfileData(profileDataArgs);
+*/
